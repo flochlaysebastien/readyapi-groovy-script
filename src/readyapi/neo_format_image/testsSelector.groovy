4d77381c57385927ed9da22a5eb205cfb7817bbf
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-04-26 22:38:15 +0200

    feat(readyapi): add a factorise neo_format_image script to spread tests

---
+++ src/readyapi/neo_format_image/testsSelector.groovy
@@ -0,0 +1,27 @@
+package readyapi.neo_format_image
+
+try {
+    log.info("Format image tests selector:")
+    log.info("    - Check image result [automatic]")
+    testRunner.runTestStepByName("Check image result")
+
+    if (context.expand('${Properties#checkFormatParameters}').toBoolean()) {
+        log.info("    - Check format parameters [required]")
+        testRunner.runTestStepByName("Check format parameters")
+    }
+
+    if (context.expand('${Properties#checkDownloadLinks}').toBoolean()) {
+        log.info("    - Check catalog download links [required]")
+        testRunner.runTestStepByName("Check catalog download links")
+    }
+
+    if (context.expand('${Properties#checkCorrelationID}').toBoolean()) {
+        log.info("    - Check correlationID [required]")
+        testRunner.runTestStepByName("Check correlationID")
+    }
+
+    testRunner.gotoStepByName("Property Transfer")
+} catch (Exception ex) {
+    log.error(ex.getMessage())
+    testRunner.fail("Failed to distribute tests")
+}
